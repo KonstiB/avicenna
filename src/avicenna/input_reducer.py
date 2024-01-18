added parameter for metric and weighting; some bug fixes

src/avicenna/input_reducer.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import normalize
 from sklearn import decomposition
 from scipy.stats import pearsonr
+import pandas as pd
+from pandas import DataFrame
 
 from fuzzingbook.Grammars import Grammar
 
@@ -22,15 +24,34 @@ from avicenna.feature_collector import (
 
 #from tests import test_features
 
-def determine_input_subset(inputs, grammar, selection_size) -> list:
-    input_feature_vector_list = generate_features(inputs, grammar, [ExistenceFeature, LengthFeature, NumericFeature])
-    input_feature_vector_list = remove_monotone_features(input_feature_vector_list)
-    normalized_input_feature_vector_list = normalize_features(input_feature_vector_list)
-    reduced_input_feature_vector_list = reduce_dimensionality(normalized_input_feature_vector_list)
-    selection_set = select_dissimilar_inputs(reduced_input_feature_vector_list, selection_size)
+def determine_input_subset(inputs, grammar, selection_size, use_PCA=False, quantitative_feature_weight=1, metric=1) -> list:
+    feature_vector_list = generate_feature_vectors(inputs, grammar, [ExistenceFeature, DerivationFeature, LengthFeature, NumericFeature])
+    feature_table = create_feature_table(feature_vector_list, inputs)
+    feature_table = remove_NaN_values(feature_table)
+    print(f"before dim reduction: {feature_table.shape}")
+    feature_table = remove_constant_features(feature_table)
+    print(f"removed constant: {feature_table.shape}")
+    feature_table = remove_redundant_features(feature_table)
+    print(f"removed redundant: {feature_table.shape}")
+    feature_table = normalize_features(feature_table, weight=quantitative_feature_weight)
 
+    if use_PCA:
+        feature_table = pca(feature_table)
+        print(f"after PCA: {feature_table.shape}")
 
-def generate_features(inputs, grammar, feature_types=[ExistenceFeature, 
+    if isinstance(metric, str):
+        if "euclid" in metric.lower():
+            metric = 2
+        elif "manhatten" in metric.lower() or "city-block" in metric.lower():
+            metric = 1
+    elif not isinstance(metric, (int, float)):
+        raise Exception("given parameter for metric can not be interpreted.")
+    selected_input_names = select_dissimilar_inputs(feature_table, selection_size, p_value=metric)
+
+    selected_inputs = create_input_list(selected_input_names, inputs)
+    return selected_inputs
+
+def generate_feature_vectors(inputs, grammar, feature_types=[ExistenceFeature, 
                                                     DerivationFeature, 
                                                     LengthFeature, 
                                                     NumericFeature]):
@@ -45,100 +66,78 @@ def generate_features(inputs, grammar, feature_types=[ExistenceFeature,
     #test_inputs = [Input.from_str(grammar, inp) for inp in inputs]
 
     collector = GrammarFeatureCollector(grammar, feature_types)
-    cnt = 0
     for test_input in test_inputs:
             feature_vector = collector.collect_features(test_input)
             #print(f"feature_vector: {feature_vector}")
             feature_list.append(feature_vector)
-            cnt = cnt + 1
 
     return feature_list
 
+def collect_feature_values(feature_vector_list, feature_name)-> np.array:
+    feature_value_array = np.zeros(len(feature_vector_list))
+    for i, input_feature_vector in enumerate(feature_vector_list):
+        feature_value_array[i] = input_feature_vector.features[feature_name]
+    return feature_value_array
 
-def remove_monotone_features(input_feature_vector_list: list()):
-    """Removes all monotone features, meaning features that have the same value for all given inputs, from the 
+def create_feature_table(feature_vector_list, inputs) -> DataFrame:
+    features = feature_vector_list[0].get_features()
+    feature_vectors = list()
+    for feature in features:
+        feature_vectors.append(collect_feature_values(feature_vector_list, feature))
+    
+    feature_names = [f"{feature}({feature.type.__name__})" for feature in features]
+    input_names = [str(input) for input in inputs]
+    data_frame = pd.DataFrame(np.transpose(feature_vectors), columns=feature_names, index=input_names , dtype=float)
+
+    return data_frame
+
+def remove_NaN_values(feature_table: DataFrame):
+    return feature_table.replace([np.inf, -np.inf, np.nan, "NaN"], 0.0)
+
+
+def normalize_features(feature_table: DataFrame, weight: float = 1):
+    """normalize column-wise each feature using min-max normalization, which results in feature values ranging between [0:1]"""
+    quantitative_rows = feature_table.filter(regex="(int)|(float)", axis="columns")
+    
+    normalized_feature_table=(quantitative_rows-quantitative_rows.min())/(quantitative_rows.max()-quantitative_rows.min()) * weight
+
+    return normalized_feature_table
+
+def remove_constant_features(feature_table: DataFrame) -> DataFrame:
+    """Removes all constant features, meaning features that have the same value for all given inputs, from the 
     input_feature_vector"""
-    feature_dict = input_feature_vector_list[0].get_features()
-    features_value = {}
-    monotone_features = list(feature_dict.keys())
-
-    for feature_name in feature_dict:
-        features_value[feature_name] =  feature_dict[feature_name]
-
-    # find monotone features (where all values are the same) 
-    for input_feature_vector in input_feature_vector_list:
-        for feature_name in feature_dict:
-            if (feature_name in monotone_features and
-                features_value[feature_name] != input_feature_vector.get_features()[feature_name]):
-                monotone_features.remove(feature_name)
+    def single_valued_cols(df):
+        df = df.to_numpy() 
+        return np.invert((df[0] == df).all(axis=0))
     
-    # remove all key-value pairs for the found monotone features
-    for input_feature_vector in input_feature_vector_list:
-        input_feature_vector.remove_features(monotone_features)
-        
-    return input_feature_vector_list
+    reduced_feature_table = feature_table.loc[:, single_valued_cols(feature_table)]
 
-def remove_redundant_features(input_vector_list: list):
-    feature_vectors = create_feature_dict(input_vector_list)
-    redundant_features = set()
-    for A, featureA in enumerate(feature_vectors):
-        for B, featureB in enumerate(feature_vectors):
-            if B <= A:
-                continue
-            if np.array_equal(feature_vectors[featureA], feature_vectors[featureB]):
-                redundant_features.add(featureB)
+    return reduced_feature_table
 
-    for input_vector in input_vector_list:
-        input_vector.remove_features(redundant_features)
+def remove_redundant_features(feature_table: DataFrame) -> DataFrame:
+    """removes redundant features, meaning features where every value is the same as for another feature, always keeps the former feature"""    
+    # remove redundant features
+    feature_table = feature_table.T.drop_duplicates().T
+    return feature_table
 
-    return input_vector_list
-
-def pca(input_feature_vector_list):
-    feature_matrix, feature_names = create_feature_matrix(input_feature_vector_list)
-    feature_matrix = np.where(np.isinf(feature_matrix), 0, feature_matrix)
-    feature_matrix = np.where(np.isnan(feature_matrix), 0, feature_matrix)
+def pca(feature_table) -> DataFrame:
+    feature_matrix = feature_table.to_numpy()
     
+    # apply z-score standardization to scale data for pca
     scaler = StandardScaler()
     scaled_feature_matrix = scaler.fit_transform(feature_matrix)
 
-
-    print(f"BEFORE PCA: {np.shape(scaled_feature_matrix)}")
+    # perform pca
     pca = decomposition.PCA(n_components='mle', svd_solver='full')
     transformed_feature_matrix = pca.fit_transform(scaled_feature_matrix)
-    print(f"AFTER PCA: {np.shape(transformed_feature_matrix)}")
-    return transformed_feature_matrix
-
-def reduce_dimensionality(input_feature_vector_list, with_PCA=False):
-    input_feature_vector_list = copy.deepcopy(input_feature_vector_list)
-    input_feature_vector_list = remove_monotone_features(input_feature_vector_list)
-    input_feature_vector_list = remove_redundant_features(input_feature_vector_list)
-
-    if with_PCA:
-        input_feature_vector_list = pca(input_feature_vector_list)
-    return input_feature_vector_list
-
-def normalize_features(input_feature_vector_list):
-    input_feature_vector_list = copy.deepcopy(input_feature_vector_list)
-    features = input_feature_vector_list[0].get_features()
-    for feature in features:
-        if feature.type == bool:
-            continue
-        feature_values = collect_feature_values(input_feature_vector_list, feature)
-        max_feature_value = np.max(feature_values)
-        min_feature_value = np.min(feature_values)
-        normed_feature_values = np.divide(feature_values - min_feature_value, max_feature_value - min_feature_value) 
-        for i in range(normed_feature_values.size):
-            input_feature_vector_list[i].features[feature] = normed_feature_values[i]
-        
-    return input_feature_vector_list
-
-
+    new_feature_table = pd.DataFrame(transformed_feature_matrix, index=feature_table.index)
+    
+    return new_feature_table
 
 def calculate_distance(x: np.array, y: np.array, p: float) -> float:
     elem_distance = np.absolute(x - y)
     elem_distance_potentiated = np.power(elem_distance, p)
-    elem_distance_potentiated = np.where(np.isinf(elem_distance_potentiated), 0, elem_distance_potentiated)
-    elem_distance_potentiated = np.where(np.isnan(elem_distance_potentiated), 0, elem_distance_potentiated)
+
 
     summed_elem_distance = np.sum(elem_distance_potentiated)
     distance = np.power(summed_elem_distance, 1/p)
@@ -146,74 +145,61 @@ def calculate_distance(x: np.array, y: np.array, p: float) -> float:
         print(f"IS NAN {x}, {y}")
     return distance
 
-def select_dissimilar_inputs(input_feature_vector_list, selection_size, preselected_inputs=None, p_value=1):
-    if len(input_feature_vector_list) < selection_size:
+def vector_norms(row_vectors: np.array, p: float) -> float:
+    vectors_potentiated = np.power(np.absolute(row_vectors), p)
+    return pow(np.sum(vectors_potentiated, axis=1), 1/p)
+
+
+def select_dissimilar_inputs(feature_table: DataFrame, selection_size: int, preselected_inputs=None, p_value=1):
+    if len(feature_table) < selection_size:
         raise Exception("Cannot produce selection that is bigger than base set!")
-    selected_inputs = preselected_inputs
-    if selected_inputs is None:
-        selected_inputs = set()
     
-    for _ in range(selection_size - len(selected_inputs)):
+    selected_input_names = preselected_inputs
+    if selected_input_names is None:
+        selected_input_names = set()
+    
+    selected_input_table = DataFrame(columns=feature_table.columns)
+    for selected_input in selected_input_names:
+        selected_input_table.loc[selected_input] = feature_table.loc[selected_input]
+        feature_table.drop(selected_input)
+    
+    for _ in range(selection_size - len(selected_input_names)):
         max_dist = 0
         max_dist_index = -1
         
-        if selected_inputs is None or len(selected_inputs) < 1:
-            origin_vector = np.zeros([len(input_feature_vector_list[0].get_features())])
-            for i, input_vector in enumerate(input_feature_vector_list):
-                input_feature_values = np.array([value for value in input_vector.get_features().values()])
-                distance = calculate_distance(input_feature_values, origin_vector, p_value)
-                if max_dist < distance:
-                    max_dist = distance
-                    max_dist_index = i
-            if max_dist_index != -1:
-                selected_inputs.add(input_feature_vector_list[max_dist_index])
+        if selected_input_names is None or len(selected_input_names) == 0:
+            # if no input has been selected so far, choose input with biggest norm
+            norms = vector_norms(feature_table.to_numpy(), p_value)
+            max_dist_index = np.argmax(norms)
+            max_dist = np.max(norms)
         else:
-            for i, input_vector in enumerate(input_feature_vector_list):
-                input_feature_values = np.array([value for value in input_vector.get_features().values()])
-                min_dist = float("inf")
-                for j, selected_vector in enumerate(selected_inputs):
-                    selected_input_feature_values = np.array([value for value in selected_vector.get_features().values()])
-                    distance = calculate_distance(input_feature_values, selected_input_feature_values, p_value)
-                    if distance < min_dist:
-                        min_dist = distance
-                if max_dist < min_dist:
-                    max_dist = min_dist
+            for i, feature_vector in enumerate(feature_table.to_numpy()):                
+                # calculate distances of all inputs to specified feature_vector
+                distances = vector_norms(selected_input_table.to_numpy(copy=True) - feature_vector, p_value)
+                min_distance = np.min(distances)
+                if max_dist < min_distance:
+                    max_dist = min_distance
                     max_dist_index = i
-            if max_dist_index != -1:
-                selected_inputs.add(input_feature_vector_list[max_dist_index])
-                #print(f"added element: {max_dist_index:02d}, max distance {max_dist}")
-            else:
-                raise Exception("No new input could be found, though list is not full.")
-    
-    return selected_inputs
+        
+        if max_dist_index >= 0 and max_dist_index < len(feature_table):
+            input = feature_table.index[max_dist_index]
+            selected_input_names.add(input)
+            selected_input_table.loc[input] = feature_table.loc[input]
+            feature_table = feature_table.drop(input)
 
-def create_Input_list(input_feature_vector_list, inputs):
+            #print(f"added element: {input}[{max_dist_index:02d}], max distance {max_dist}, list size: {len(selected_input_names)}/{selected_input_table.shape}, {np.shape(feature_matrix)}/{feature_table.shape}")
+        else:
+            raise Exception("No new input could be found, though list is not full.")
+    
+    return selected_input_names
+
+def create_input_list(selected_input_names, inputs):
     input_subset = set()
-    for input_feature_vector in input_feature_vector_list:
+    for selected_input_name in selected_input_names:
         for input in inputs:
-            if str(input) == input_feature_vector.test_input:
+            if str(input) == selected_input_name:
                 input_subset.add(input)
     return input_subset
-
-def collect_feature_values(input_feature_vector_list, feature_name)-> np.array:
-    feature_value_array = np.zeros(len(input_feature_vector_list))
-    for i, input_feature_vector in enumerate(input_feature_vector_list):
-        feature_value_array[i] = input_feature_vector.features[feature_name]
-    return feature_value_array
-
-def create_feature_dict(input_vector_list)-> dict():
-    features = input_vector_list[0].get_features()
-    feature_vectors = dict()
-    for feature in features:
-        feature_vectors[feature]= collect_feature_values(input_vector_list, feature)
-    return feature_vectors
-
-def create_feature_matrix(input_vector_list):
-    features = input_vector_list[0].get_features()
-    feature_vectors = list()
-    for feature in features:
-        feature_vectors.append(collect_feature_values(input_vector_list, feature))
-    return np.transpose(np.array(feature_vectors)), np.array(features)
 
 if __name__ == "__main__":
     pass