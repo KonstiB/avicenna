found a bug, returning only quantitative features from normalization;optimized the code, to accommodate much slower runtime after fixing aforementioned bug; added statistical evaluation workflow in jupyter notebook

src/avicenna/input_reducer.py
@@ -24,20 +24,30 @@ from avicenna.feature_collector import (
 
 #from tests import test_features
 
-def determine_input_subset(inputs, grammar, selection_size, use_PCA=False, quantitative_feature_weight=1, metric=1) -> list:
-    feature_vector_list = generate_feature_vectors(inputs, grammar, [ExistenceFeature, DerivationFeature, LengthFeature, NumericFeature])
+def determine_input_subset(inputs, 
+                           grammar, 
+                           selection_size, 
+                           mode="dissimilar", 
+                           feature_types=[ExistenceFeature, DerivationFeature, LengthFeature, NumericFeature], 
+                           use_PCA=False, 
+                           quantitative_feature_weight=1, 
+                           metric=1) -> list:
+    
+
+    feature_vector_list = generate_feature_vectors(inputs, grammar, feature_types)
     feature_table = create_feature_table(feature_vector_list, inputs)
     feature_table = remove_NaN_values(feature_table)
-    print(f"before dim reduction: {feature_table.shape}")
+    #print(f"before dim reduction: {feature_table.shape}")
     feature_table = remove_constant_features(feature_table)
-    print(f"removed constant: {feature_table.shape}")
+    #print(f"removed constant: {feature_table.shape}")
     feature_table = remove_redundant_features(feature_table)
-    print(f"removed redundant: {feature_table.shape}")
+    #print(f"removed redundant: {feature_table.shape}")
     feature_table = normalize_features(feature_table, weight=quantitative_feature_weight)
+    #print(f"after normalization: {feature_table.shape}")
 
     if use_PCA:
         feature_table = pca(feature_table)
-        print(f"after PCA: {feature_table.shape}")
+        #print(f"after PCA: {feature_table.shape}")
 
     if isinstance(metric, str):
         if "euclid" in metric.lower():
@@ -46,9 +56,11 @@ def determine_input_subset(inputs, grammar, selection_size, use_PCA=False, quant
             metric = 1
     elif not isinstance(metric, (int, float)):
         raise Exception("given parameter for metric can not be interpreted.")
-    selected_input_names = select_dissimilar_inputs(feature_table, selection_size, p_value=metric)
+    selected_input_names = select_input_subset(feature_table, selection_size, p_value=metric, mode= mode)
 
     selected_inputs = create_input_list(selected_input_names, inputs)
+    
+    #print(f"at the end: {feature_table.shape}")
     return selected_inputs
 
 def generate_feature_vectors(inputs, grammar, feature_types=[ExistenceFeature, 
@@ -98,10 +110,11 @@ def remove_NaN_values(feature_table: DataFrame):
 def normalize_features(feature_table: DataFrame, weight: float = 1):
     """normalize column-wise each feature using min-max normalization, which results in feature values ranging between [0:1]"""
     quantitative_rows = feature_table.filter(regex="(int)|(float)", axis="columns")
+    qualitative_rows = feature_table.drop(columns=quantitative_rows, errors="ignore")
     
     normalized_feature_table=(quantitative_rows-quantitative_rows.min())/(quantitative_rows.max()-quantitative_rows.min()) * weight
 
-    return normalized_feature_table
+    return pd.concat([normalized_feature_table, qualitative_rows], axis=1)
 
 def remove_constant_features(feature_table: DataFrame) -> DataFrame:
     """Removes all constant features, meaning features that have the same value for all given inputs, from the 
@@ -145,12 +158,29 @@ def calculate_distance(x: np.array, y: np.array, p: float) -> float:
         print(f"IS NAN {x}, {y}")
     return distance
 
-def vector_norms(row_vectors: np.array, p: float) -> float:
-    vectors_potentiated = np.power(np.absolute(row_vectors), p)
-    return pow(np.sum(vectors_potentiated, axis=1), 1/p)
+def select_input_subset(feature_table: DataFrame, selection_size: int, preselected_inputs=None, p_value=1, mode = "dissimilar"):
+    
+    def norm_vectors(row_vectors: np.array, p: float):
+        if isinstance(row_vectors, pd.DataFrame):
+            row_vectors = row_vectors.to_numpy()
+        if float(p) == 1.0:
+            return np.sum(np.absolute(row_vectors), axis=1)
+        else:  
+            vectors_potentiated = np.power(np.absolute(row_vectors), p)
+            return np.power(np.sum(vectors_potentiated, axis=1), 1.0/float(p))
 
+    def pairwise_distances(vectors, p_value):
+        if isinstance(vectors, pd.DataFrame):
+            vectors = vectors.to_numpy()
+        num_vectors = len(vectors)
+        distances = np.zeros((num_vectors, num_vectors), dtype=float)
+        for i in range(num_vectors):
+                distances[i] = norm_vectors(vectors[i] - vectors, p_value)
+        
+        np.fill_diagonal(distances, float("nan"))
+        return distances
 
-def select_dissimilar_inputs(feature_table: DataFrame, selection_size: int, preselected_inputs=None, p_value=1):
+    
     if len(feature_table) < selection_size:
         raise Exception("Cannot produce selection that is bigger than base set!")
     
@@ -158,38 +188,50 @@ def select_dissimilar_inputs(feature_table: DataFrame, selection_size: int, pres
     if selected_input_names is None:
         selected_input_names = set()
     
-    selected_input_table = DataFrame(columns=feature_table.columns)
-    for selected_input in selected_input_names:
-        selected_input_table.loc[selected_input] = feature_table.loc[selected_input]
-        feature_table.drop(selected_input)
+    selected_input_index = [feature_table.index.get_loc(name) for name in selected_input_names]
     
-    for _ in range(selection_size - len(selected_input_names)):
-        max_dist = 0
-        max_dist_index = -1
-        
-        if selected_input_names is None or len(selected_input_names) == 0:
-            # if no input has been selected so far, choose input with biggest norm
-            norms = vector_norms(feature_table.to_numpy(), p_value)
-            max_dist_index = np.argmax(norms)
-            max_dist = np.max(norms)
-        else:
-            for i, feature_vector in enumerate(feature_table.to_numpy()):                
-                # calculate distances of all inputs to specified feature_vector
-                distances = vector_norms(selected_input_table.to_numpy(copy=True) - feature_vector, p_value)
-                min_distance = np.min(distances)
-                if max_dist < min_distance:
-                    max_dist = min_distance
-                    max_dist_index = i
-        
-        if max_dist_index >= 0 and max_dist_index < len(feature_table):
-            input = feature_table.index[max_dist_index]
-            selected_input_names.add(input)
-            selected_input_table.loc[input] = feature_table.loc[input]
-            feature_table = feature_table.drop(input)
+    # preprocess all distances and norms to save time
+    vector_norms = norm_vectors(feature_table, p_value)
+    vector_pair_distances = pairwise_distances(feature_table.to_numpy(), p_value=p_value)
 
-            #print(f"added element: {input}[{max_dist_index:02d}], max distance {max_dist}, list size: {len(selected_input_names)}/{selected_input_table.shape}, {np.shape(feature_matrix)}/{feature_table.shape}")
+    for selection_num in range(selection_size - len(selected_input_index)):
+        best_dist = 0
+        best_dist_index = -1
+        
+        if selected_input_index is None or len(selected_input_index) == 0:
+            # if no input has been selected so far, choose input with biggest/smallest norm
+            if mode == "dissimilar":
+                best_dist_index = np.argmax(vector_norms)
+                best_dist = np.max(vector_norms)
+            elif mode == "similar":
+                best_dist_index = np.argmin(vector_norms)
+                best_dist = np.min(vector_norms)
         else:
-            raise Exception("No new input could be found, though list is not full.")
+            #[x for x in main_list if x not in elements_to_remove]
+            for i in [index for index in range(len(feature_table.index)) if index not in selected_input_index]:                
+                # retrieve distances of all selected inputs to specified feature_vector
+                vector_index = [i] * len(selected_input_index)
+                distances = vector_pair_distances[vector_index, selected_input_index]
+                if mode == "dissimilar": 
+                    min_distance = np.nanmin(distances)
+                    if best_dist < min_distance:
+                        best_dist = min_distance
+                        best_dist_index = i
+                elif mode == "similar":
+                    max_distance = np.nanmax(distances)
+                    if best_dist > max_distance:
+                        best_dist = max_distance
+                        best_dist_index = i
+
+        if best_dist_index >= 0 and best_dist_index < len(feature_table):
+            selected_input_index.append(best_dist_index)
+            input = feature_table.index[best_dist_index]
+            selected_input_names.add(input)
+            #selected_input_table.loc[input] = feature_table.loc[input]
+            #feature_table = feature_table.drop(input)
+
+        else:
+            raise Exception(f"""No new input could be found, though list is not full.\n Index:{best_dist_index}, best distance: {best_dist}\nCurrent iteration: {selection_num}, Max iterations: {selection_size}""")
     
     return selected_input_names
 